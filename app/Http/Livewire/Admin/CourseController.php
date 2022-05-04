952b5e5f97389eb1285023962f0030a118de6c59
Carlos Farid Nogales López <34426099+FaruNL@users.noreply.github.com>
2022-05-04 01:15:30 -0500

    style: Corrige code style

--- app/Http/Livewire/Admin/CourseController.php
+++ app/Http/Livewire/Admin/CourseController.php
@@ -173,7 +173,7 @@ class CourseController extends Component
             'courses' => Course::query()
                 ->when($this->filters['modalidad'], fn ($query, $modalidad) => $query->where('modalidad', $modalidad))
                 ->when($this->filters['perfil'], fn ($query, $perfil) => $query->where('perfil', $perfil))
-                ->when($this->search, fn($query, $search) => $query->where('nombre', 'like', "%$this->search%"))
+                ->when($this->search, fn ($query, $search) => $query->where('nombre', 'like', "%$this->search%"))
                 ->orderBy($this->sortField, $this->sortDirection)
                 ->paginate($this->perPage),
         ]);
