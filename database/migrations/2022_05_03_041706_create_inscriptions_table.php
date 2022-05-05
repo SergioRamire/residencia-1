237b8f8c44f5bb8216171e91b6e24e8a5bdff4c4
natali <nat.casro@gmail.com>
2022-05-04 21:58:12 -0500

    feat: Agregué PK id en tablas inscriptions y groupassignments

--- database/migrations/2022_05_03_041706_create_inscriptions_table.php
+++ database/migrations/2022_05_03_041706_create_inscriptions_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('inscriptions', function (Blueprint $table) {
+            $table->id();
             $table->double('calificacion');
             $table->string('estatus');
             $table->boolean('asistencias_minimas');
