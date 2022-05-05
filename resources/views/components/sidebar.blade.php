655daf021befd3779fa927794a1564b2eaf19735
RouterUIZ007 <router.uiz.360@gmail.com>
2022-05-05 03:05:23 -0500

    feat: siderbar perfil

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -51,9 +51,15 @@
         <!-- Enlaces de navegación -->
         <div class="mt-5 flex-1 h-0 overflow-y-auto">
             <nav class="px-2 space-y-1">
+                
                 <x-sidebar.link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                     <x-slot name="icon"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></x-slot>
                     Dashboard
+                </x-sidebar.link>
+                
+                <x-sidebar.link :href='route("datos.personales")' :active='request()->route("datos.personales")'>
+                    <x-slot name="icon"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" /></x-slot>
+                    Perfil
                 </x-sidebar.link>
 
                 <x-sidebar.dropdown title="E-commerce 1" dp-id="1">
@@ -127,7 +133,10 @@
                     <x-slot name="icon"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></x-slot>
                     Dashboard
                 </x-sidebar.link>
-
+                <x-sidebar.link :href='route("datos.personales")' :active='request()->route("datos.personales")'>
+                    <x-slot name="icon"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" /></x-slot>
+                    Perfil
+                </x-sidebar.link>
                 <x-sidebar.dropdown title="E-commerce 1" dp-id="1">
                     <x-slot name="icon" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"/>
