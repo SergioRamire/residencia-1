d4290d3585afa13842a42530f1b912e0300bf60f
RouterUIZ007 <router.uiz.360@gmail.com>
2022-05-06 12:23:56 -0500

    fix: datos laboral, save, validacion curp

--- app/Http/Livewire/Admin/Datos/Personales.php
+++ app/Http/Livewire/Admin/Datos/Personales.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Livewire\Admin\Datos;
 
-use App\Models\Area;
 use App\Models\User;
+use App\Models\Area;
 use Livewire\Component;
 
 class Personales extends Component
@@ -21,51 +21,28 @@ class Personales extends Component
     public function rules()
     {
         return [
-            'user.rfc' => 'required|alpha_num|max:254',
-            'user.curp' => 'required|alpha_num|max:254',
-            'user.name' => 'required|string|max:254',
-            'user.apellido_materno' => 'required|alpha|max:254',
-            'user.apellido_paterno' =>'required|alpha|max:254',
-            'user.sexo' => 'required|alpha',
-            'user.email' => 'required|email|max:254',
-            'user.correo_tecnm' => 'required|email|max:254',
-            'user.estudio_maximo' => 'required|string|max:254',
-            'user.carrera' => 'required|string|max:254',
+            'user.rfc' => ['required', 'regex:/^[a-zA-zÑñ]{3,4}[0-9]{6}[a-zA-ZÑñ]{3}$/u', 'max:254'],
+            'user.curp' => ['required', 'regex:/^[a-zA-ZñÑ]{1}[AEIOUaeiou]{1}[a-zA-ZñÑ]{2}[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])[H|M|h|m](AS|BC|BS|CC|CS|CH|CL|CM|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)[B-DF-HJ-NP-TV-Z]{3}[0-9A-Z]{1}[0-9]{1}$/u', 'max:254'],
+            'user.name' => ['required', 'regex:/^[\pL\pM\s]+$/u', 'max:255'],
+            'user.apellido_materno' => ['required', 'regex:/^[\pL\pM\s]+$/u', 'max:255'],
+            'user.apellido_paterno' => ['required', 'regex:/^[\pL\pM\s]+$/u', 'max:255'],
+            'user.sexo' => ['required', 'regex:/^[F|M]$/u', 'max:1'],
+            'user.email' => ['required', 'email', 'max:254'],
+            'user.correo_tecnm' => ['required', 'email', 'max:254'],
+            'user.estudio_maximo' => ['required', 'regex:/^[\pL\pM\s]+$/u', 'max:255'],
+            'user.carrera' => ['required', 'regex:/^[\pL\pM\s]+$/u', 'max:255'],
 
-            'user.area_id' =>'required',
-            'user.clave_presupuestal' => 'required|alpha_num|max:254',
-            'user.puesto' => 'required|string|max:254',
+            'user.area_id' => 'required',
+            'user.clave_presupuestal'  => 'required',
+            'user.puesto'  => 'required',
             'area.telefono' => 'required|numeric',
-            'area.jefe' => 'required|string|max:254',
+            'area.jefe'  => 'required',
             'user.hora_entrada' => 'required',
             'user.hora_salida' => 'required',
-            'user.tipo' =>'required|alpha_dash|string|max:254',
-            'user.organizacion_origen' => 'required|string|max:254',
-            'user.cuenta_moodle' => 'required',
+            'user.tipo'  => 'required',
+            'user.organizacion_origen'  => 'required',
+            'user.cuenta_moodle'  => 'required',
         ];
-        // return [
-        //     'user.rfc' => 'required|regex:[A-ZÑ&]{3,4}\d{6}(?:[A-Z\d]{3})?$|max:254',
-        //     'user.curp' => 'required|regex:([A-ZÑ&]|[a-zñ&]{1})([AEIOU]|[aeiou]{1})([A-Z&]|[a-z&]{1})([A-Z&]|[a-z&]{1})([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])([HM]|[hm]{1})([AS|as|BC|bc|BS|bs|CC|cc|CS|cs|CH|ch|CL|cl|CM|cm|DF|df|DG|dg|GT|gt|GR|gr|HG|hg|JC|jc|MC|mc|MN|mn|MS|ms|NT|nt|NL|nl|OC|oc|PL|pl|QT|qt|QR|qr|SP|sp|SL|sl|SR|sr|TC|tc|TS|ts|TL|tl|VZ|vz|YN|yn|ZS|zs|NE|ne]{2})([A|a|E|e|I|i|O|o|U|u]{1})([A|a|E|e|I|i|O|o|U|u]{1})([A|a|E|e|I|i|O|o|U|u]{1})([0-9]{2})$|max:254',
-        //     'user.name' => 'required|regex:[A-Z,Ñ,a-z][A-Z,a-z, ,ñ,Ñ,á,é,í,ó,ú,Á,É,Í,Ó,Ú]+$|max:254',
-        //     'user.apellido_materno' => 'required|regex:[A-Z,Ñ,a-z][A-Z,a-z, ,ñ,Ñ,á,é,í,ó,ú,Á,É,Í,Ó,Ú]+$|max:254',
-        //     'user.apellido_paterno' => 'required|regex:[A-Z,Ñ,a-z][A-Z,a-z, ,ñ,Ñ,á,é,í,ó,ú,Á,É,Í,Ó,Ú]+$|max:254',
-        //     'user.sexo' => 'required|in:M,F|max:1',
-        //     'user.email' => 'required|regex:(.*)@itoaxaca\.edu\.mx$i|max:255',
-        //     'user.correo_tecnm' => 'required|regex:(.*)@oaxaca\.tecnm\.mx$i|max:255',
-        //     'user.estudio_maximo' => 'required|regex:[A-Z,Ñ,a-z][A-Z,a-z, ,ñ,Ñ,á,é,í,ó,ú,Á,É,Í,Ó,Ú,.]+$|max:254',
-        //     'user.carrera' => 'required|regex:[A-Z,Ñ,a-z][A-Z,a-z, ,ñ,Ñ,á,é,í,ó,ú,Á,É,Í,Ó,Ú]+$|max:254',
-
-        //     'user.area_id' => 'required|int',
-        //     'user.clave_presupuestal' => 'required|regex:[0-9A-Z,a-z,ñ,Ñ,á,é,í,ó,ú,Á,É,Í,Ó,Ú,.]+$|max:254',
-        //     'user.puesto' => 'required|regex:[A-Z,Ñ,a-z][A-Z,a-z, ,ñ,Ñ,á,é,í,ó,ú,Á,É,Í,Ó,Ú,.]+$|max:254',
-        //     'area.telefono' => 'required|regex:[0-9]+$|max:10',
-        //     'area.jefe' => 'required|regex:[A-Z,Ñ,a-z][A-Z,a-z, ,ñ,Ñ,á,é,í,ó,ú,Á,É,Í,Ó,Ú]+$|max:254',
-        //     'user.hora_entrada' => 'required',
-        //     'user.hora_salida' => 'required',
-        //     'user.tipo' => 'required|regex:[A-Z,Ñ,a-z][A-Z,a-z, ,ñ,Ñ,á,é,í,ó,ú,Á,É,Í,Ó,Ú]+$|max:254',
-        //     'user.organizacion_origen' => 'required|regex:[A-Z,Ñ,a-z][A-Z,a-z, ,ñ,Ñ,á,é,í,ó,ú,Á,É,Í,Ó,Ú,.]+$|max:254',
-        //     'user.cuenta_moodle' => 'required|in:1,2|max:1',
-        // ];
     }
 
     public function render()
@@ -82,7 +59,6 @@ class Personales extends Component
     public function save()
     {
         $this->user->save();
-        $this->area->save();
 
         $this->showConfirmationModal = false;
 
--- resources/views/livewire/admin/datos/personales.blade.php
+++ resources/views/livewire/admin/datos/personales.blade.php
@@ -16,44 +16,10 @@
     
 
             </div>
-{{-- 
-            <div class="bg-white shadow overflow-hidden sm:rounded-lg">
-                <div class="px-4 py-5 sm:px-6">
-                  <h3 class="text-lg leading-6 font-medium text-gray-900">Cor</h3>
-                  <p class="mt-1 max-w-2xl text-sm text-gray-500">{{ auth()->user()->email }}</p>
-                </div>
-                <div class="border-t border-gray-200">
-                  <dl>
-                    <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                      <dt class="text-sm font-medium text-gray-500">Full name</dt>
-                      <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Margot Foster</dd>
-                    </div>
-                    <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                      <dt class="text-sm font-medium text-gray-500">Application for</dt>
-                      <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Backend Developer</dd>
-                    </div>
-                    <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                      <dt class="text-sm font-medium text-gray-500">Email address</dt>
-                      <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">margotfoster@example.com</dd>
-                    </div>
-                    <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                      <dt class="text-sm font-medium text-gray-500">Salary expectation</dt>
-                      <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">$120,000</dd>
-                    </div>
-                    
-                  </dl>
-                </div>
-            </div> --}}
-
-
-
-
-        </div>
-
         
     </x-slot>
 
-    
+
 
     <div class="max-w-7xl mx-auto pt-5 pb-5">
         <div class="space-y-2">
@@ -139,6 +105,7 @@
     </div>
 
 
+    
     <div class="max-w-7xl mx-auto pt-5 pb-5">
         <div class="space-y-2">
             <div class="p-4 bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 lg:p-8 ">
@@ -149,7 +116,7 @@
                         {{-- Nombre de area --}}
                         <div class="mt-4 flex-1">
                             <x-jet-label for="areanombre" value="Nombre de Area" />
-                                <x-input.select wire:model.defer="user.area_id" id="areanombre" class="mt-1 w-full" name="areanombre" for="user.area_id" required>
+                                <x-input.select wire:model.defer="user.area_id" id="areanombre" class="mt-1 w-full" name="areanombre" for="user.area_id" readonly>
                                     @foreach(App\Models\Area::all() as $area)
                                         <option value="{{ $area->id }}"> {{ $area->nombre }} </option>
                                     @endforeach
@@ -158,7 +125,7 @@
                         {{-- clave_presupuestal --}}
                         <div class="mt-4 flex-1">
                             <x-jet-label for="clave_presupuestal" value="Clave Presupuestal" />
-                            <x-input.error wire:model.defer="user.clave_presupuestal" class="block mt-1 w-full" type="text" id="clave_presupuestal" name="clave_presupuestal" for="user.clave_presupuestal" required />
+                            <x-input.error wire:model.defer="user.clave_presupuestal" class="block mt-1 w-full" type="text" id="clave_presupuestal" name="clave_presupuestal" for="user.clave_presupuestal" readonly />
                         </div>
                     </div>
                     <div class="flex flex-col sm:flex-row sm:items-baseline sm:gap-x-1.5"> 
@@ -177,25 +144,24 @@
                         <!-- puesto -->
                         <div class="mt-4 flex-1">
                             <x-jet-label for="puesto" value="Puesto" />
-                            <x-input.error wire:model.defer="user.puesto" class="block mt-1 w-full" type="text" id="puesto" name="puesto" for="user.puesto" required />
+                            <x-input.error wire:model.defer="user.puesto" class="block mt-1 w-full" type="text" id="puesto" name="puesto" for="user.puesto" readonly />
                         </div>       
                         {{-- hora_entrada --}}
                         <div class="mt-4 flex-1">
                             <x-jet-label for="hora_entrada" value="Hora de Entrada" />
-                            <x-input.error wire:model.defer="user.hora_entrada" class="block mt-1 w-full" type="time" id="hora_entrada" name="hora_entrada" for="user.hora_entrada" required />
+                            <x-input.error wire:model.defer="user.hora_entrada" class="block mt-1 w-full" type="time" id="hora_entrada" name="hora_entrada" for="user.hora_entrada" readonly />
                         </div>
                         {{-- hora_salida --}}
                         <div class="mt-4 flex-1">
                             <x-jet-label for="hora_salida" value="Hora de Salida" />
-                            <x-input.error wire:model.defer="user.hora_salida" class="block mt-1 w-full" type="time" id="hora_salida" name="hora_salida" for="user.hora_salida" required />
+                            <x-input.error wire:model.defer="user.hora_salida" class="block mt-1 w-full" type="time" id="hora_salida" name="hora_salida" for="user.hora_salida" readonly />
                         </div>
                     </div>
                     <div class="flex flex-col sm:flex-row sm:items-baseline sm:gap-x-1.5">
                         {{-- tipo --}}
                         <div class="mt-4 flex-1">
                             <x-jet-label for="tipo" value="Tipo" />
-                            <x-input.select wire:model.defer="user.tipo" id="sexo" class="mt-1 w-full" name="sexo" for="user.tipo" required>
-                                <option value="" disabled>Selecciona Tipo...</option>
+                            <x-input.select wire:model.defer="user.tipo" id="sexo" class="mt-1 w-full" name="sexo" for="user.tipo" readonly>
                                 <option value="Base">Base</option>
                                 <option value="Interinato">Interinato</option>
                                 <option value="Contrato">Contrato</option>
@@ -204,30 +170,25 @@
                         {{-- organizacion_origen --}}
                         <div class="mt-4 flex-1">
                             <x-jet-label for="organizacion_origen" value="Organizacion de Origen" />
-                            <x-input.error wire:model.defer="user.organizacion_origen" class="block mt-1 w-full" type="text" id="organizacion_origen" name="organizacion_origen" for="user.organizacion_origen" required />
+                            <x-input.error wire:model.defer="user.organizacion_origen" class="block mt-1 w-full" type="text" id="organizacion_origen" name="organizacion_origen" for="user.organizacion_origen" readonly />
                         </div>
                         {{-- cuenta_moodle --}}
+
+
                         <div class="mt-4 flex-1">
                             <x-jet-label for="cuenta_moodle" value="Cuenta Moodle" />
-                            <x-input.select wire:model.defer="user.cuenta_moodle" id="cuenta_moodle" class="mt-1 w-full" name="cuenta_moodle" for="user.cuenta_moodle" required>
-                                <option value="" disabled>Selecciona si Tiene Cuenta...</option>
-                                <option value="0">No Tiene</option>
-                                <option value="1">Si Tiene</option>
-                            </x-input.select>
-                        </div>
-                    </div>
-                    
-                    <div class="flex flex-row-reverse">
-                        <div class="mt-4 flex-1">
-                            <x-jet-button class="ml-3" wire:click="confirmSave()" type="button">
-                                Guardar Cambios
-                            </x-jet-button>
+
+                            <x-input.error wire:model.defer="user.cuenta_moodle" class="block mt-1 w-full" type="text" id="cuenta_moodle" name="cuenta_moodle" for="user.cuenta_moodle" readonly />
+
+
+
                         </div>
                     </div>
                 </form>
             </div>
         </div>
     </div>
+    
     <!-- Modales -->
     {{-- @include('livewire.admin.instructor.edit_create') --}}
     {{-- @include('livewire.admin.instructor.show') --}}
