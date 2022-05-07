4878b8369d086deabb107e26e93adf0af09bc37b
Carlos Farid Nogales López <34426099+FaruNL@users.noreply.github.com>
2022-05-07 15:33:45 -0500

    fix: Agrupa rutas por un solo permiso '*.show'
    Si el usuario quiere acceder a una ruta con la que solo cuenta con un
    permiso listado en los middlewares, no lo dejaría. Pues tendría que
    tener sí o sí todos los permisos listados.

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,9 @@ Route::get('/', function () {
 Route::middleware(['auth:web', config('jetstream.auth_session'), 'verified'])->group(function () {
     Route::get('/dashboard', fn () => view('dashboard'))->name('dashboard');
 
-    Route::middleware(['can:user.show', 'can:role.show'])->group(function () {
-        Route::prefix('admin')->name('admin.')->group(function () {
-            Route::get('usuarios', \App\Http\Livewire\Admin\UserController::class)->name('usuarios');
-            Route::get('roles', \App\Http\Livewire\Admin\RoleController::class)->name('roles');
-        });
-    });
+    Route::middleware('can:user.show')->prefix('admin')->name('admin.')
+        ->get('usuarios', \App\Http\Livewire\Admin\UserController::class)->name('usuarios');
+
+    Route::middleware('can:role.show')->prefix('admin')->name('admin.')
+        ->get('roles', \App\Http\Livewire\Admin\RoleController::class)->name('roles');
 });
