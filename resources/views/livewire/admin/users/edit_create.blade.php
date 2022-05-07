53cb80863b06631635240586019490fbdd14fb66
Carlos Farid Nogales López <34426099+FaruNL@users.noreply.github.com>
2022-05-07 01:08:06 -0500

    refactor: Reorganiza la disposición de los inputs

--- resources/views/livewire/admin/users/edit_create.blade.php
+++ resources/views/livewire/admin/users/edit_create.blade.php
@@ -4,40 +4,49 @@
     </x-slot>
     <x-slot name="content">
         <form wire:submit.prevent="confirmSave()" id="userForm">
+            <!-- Nombre y Apellidos -->
             <div class="flex flex-col sm:flex-row sm:items-baseline sm:gap-x-1.5">
+                <!-- Nombre -->
                 <div class="sm:flex-1">
-                    <!-- Nombre -->
                     <x-jet-label for="nombre" value="Nombre"/>
                     <x-input.error wire:model.defer="user.name" class="block mt-1 w-full" type="text" id="nombre" name="nombre" for="user.name" required/>
                 </div>
-                <div class="sm:flex-1">
-                    <!-- Apellido paterno -->
+
+                <!-- Apellido paterno -->
+                <div class="mt-4 sm:mt-0 sm:flex-1">
                     <x-jet-label for="apellido_paterno" value="Apellido paterno"/>
                     <x-input.error wire:model.defer="user.apellido_paterno" class="block mt-1 w-full" type="text" id="apellido_paterno" name="apellido_paterno" for="user.apellido_paterno"/>
                 </div>
-                <div class="sm:flex-1">
-                    <!-- Apellido materno -->
+
+                <!-- Apellido materno -->
+                <div class="mt-4 sm:mt-0 sm:flex-1">
                     <x-jet-label for="apellido_materno" value="Apellido materno"/>
                     <x-input.error wire:model.defer="user.apellido_materno" class="block mt-1 w-full" type="text" id="apellido_materno" name="apellido_materno" for="user.apellido_materno"/>
                 </div>
             </div>
 
-            <!-- Correo -->
-            <div class="mt-4">
-                <x-jet-label for="email" value="Correo" />
-                <x-input.error wire:model.defer="user.email" class="block mt-1 w-full" type="email" id="email" name="email" for="user.email" required/>
+            <!-- Correo y rol -->
+            <div class="flex flex-col sm:flex-row sm:items-baseline sm:gap-x-1.5">
+                <!-- Correo -->
+                <div class="mt-4 sm:flex-1">
+                    <x-jet-label for="email" value="Correo" />
+                    <x-input.error wire:model.defer="user.email" class="block mt-1 w-full" type="email" id="email" name="email" for="user.email" required/>
+                </div>
             </div>
 
-            <!-- Contraseña -->
-            <div class="mt-4">
-                <x-jet-label for="contraseña" value="{{ $edit ? 'Contraseña nueva' : 'Contraseña' }}" />
-                <x-input.error wire:model.defer="password" class="block mt-1 w-full" type="password" id="contraseña" name="contraseña" for="password"/>
-            </div>
+            <!-- Contraseñas -->
+            <div class="flex flex-col sm:flex-row sm:items-baseline sm:gap-x-1.5">
+                <!-- Contraseña -->
+                <div class="mt-4 sm:flex-1">
+                    <x-jet-label for="contraseña" :value="$edit ? 'Contraseña nueva' : 'Contraseña'" />
+                    <x-input.error wire:model.defer="password" class="block mt-1 w-full" type="password" id="contraseña" name="contraseña" for="password"/>
+                </div>
 
-            <!-- Confirmación de contraseña -->
-            <div class="mt-4">
-                <x-jet-label for="contraseña_confirmation" value="Confirmación de contraseña" />
-                <x-input.error wire:model.defer="password_confirmation" class="block mt-1 w-full" type="password" id="contraseña_confirmation" name="contraseña_confirmation" for="password_confirmation"/>
+                <!-- Confirmación de contraseña -->
+                <div class="mt-4 sm:flex-1">
+                    <x-jet-label for="contraseña_confirmation" value="Confirmación de contraseña" />
+                    <x-input.error wire:model.defer="password_confirmation" class="block mt-1 w-full" type="password" id="contraseña_confirmation" name="contraseña_confirmation" for="password_confirmation"/>
+                </div>
             </div>
         </form>
     </x-slot>
