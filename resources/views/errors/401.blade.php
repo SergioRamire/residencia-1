92371f98899d8f12ce60babed463a3dbb02e5135
Carlos Farid Nogales López <34426099+FaruNL@users.noreply.github.com>
2022-05-07 21:11:43 -0500

    feat: Añade páginas de errores personalizadas

---
+++ resources/views/errors/401.blade.php
@@ -0,0 +1,6 @@
+@extends('errors.layout')
+
+@section('title', 'Sin autenticar')
+@section('code', '401')
+@section('message', 'Sin autenticar')
+@section('description', 'Sólo se permite el acceso a los usuarios registrados.')
