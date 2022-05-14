42e4370bcd616d1d06ceb5adb95a4fc1b72e45c2
Carlos Farid Nogales López <34426099+FaruNL@users.noreply.github.com>
2022-05-14 14:27:00 -0500

    fix: Aplica model.defer a inputs

--- resources/views/livewire/admin/participants/edit.blade.php
+++ resources/views/livewire/admin/participants/edit.blade.php
@@ -9,13 +9,13 @@
                 <!-- RFC -->
                 <div class="sm:flex-1">
                     <x-jet-label for="rfc" value="RFC"/>
-                    <x-input.error wire:model="user.rfc" class="block mt-1 w-full" type="text" id="rfc" name="rfc" for="user.rfc" required/>
+                    <x-input.error wire:model.defer="user.rfc" class="block mt-1 w-full" type="text" id="rfc" name="rfc" for="user.rfc" required/>
                 </div>
 
                 <!-- CURP -->
                 <div class="sm:flex-1">
                     <x-jet-label for="curp" value="CURP"/>
-                    <x-input.error wire:model="user.curp" class="block mt-1 w-full" type="text" id="curp" name="curp" for="user.curp" required/>
+                    <x-input.error wire:model.defer="user.curp" class="block mt-1 w-full" type="text" id="curp" name="curp" for="user.curp" required/>
                 </div>
             </div>
 
@@ -45,7 +45,7 @@
                 <!-- Sexo -->
                 <div class="mt-4 sm:flex-1">
                     <x-jet-label for="sexo" value="Sexo"/>
-                    <x-input.select wire:model="user.sexo" class="mt-1 w-full" id="sexo" name="sexo" required>
+                    <x-input.select wire:model.defer="user.sexo" class="mt-1 w-full" id="sexo" name="sexo" required>
                         <option value="" disabled>Selecciona el sexo</option>
                         <option value="F">Femenino</option>
                         <option value="M">Masculino</option>
@@ -56,7 +56,7 @@
                 <!-- Tipo -->
                 <div class="mt-4 sm:flex-1">
                     <x-jet-label for="tipo" value="Tipo"/>
-                    <x-input.select wire:model="user.tipo" class="mt-1 w-full" id="tipo" name="tipo" required>
+                    <x-input.select wire:model.defer="user.tipo" class="mt-1 w-full" id="tipo" name="tipo" required>
                         <option value="" disabled>Selecciona el tipo</option>
                         <option value="Base">Base</option>
                         <option value="Interinato">Interinato</option>
@@ -68,7 +68,7 @@
                 <!-- Clave -->
                 <div class="mt-4 sm:flex-1">
                     <x-jet-label for="clave_presupuestal" value="Clave presupuestal"/>
-                    <x-input.error wire:model="user.clave_presupuestal" class="block mt-1 w-full" type="text" id="clave_presupuestal" name="clave" for="user.clave_presupuestal" required/>
+                    <x-input.error wire:model.defer="user.clave_presupuestal" class="block mt-1 w-full" type="text" id="clave_presupuestal" name="clave" for="user.clave_presupuestal" required/>
                 </div>
             </div>
 
@@ -81,7 +81,7 @@
             <!-- Carrera-->
             <div class="mt-4">
                 <x-jet-label for="carrera" value="Carrera"/>
-                <x-input.error wire:model="user.carrera" class="block mt-1 w-full" type="text" id="carrera" name="carrera" for="user.carrera" required/>
+                <x-input.error wire:model.defer="user.carrera" class="block mt-1 w-full" type="text" id="carrera" name="carrera" for="user.carrera" required/>
             </div>
 
             <!-- Correo ITO y TECNM -->
@@ -89,13 +89,13 @@
                 <!-- Correo ITO -->
                 <div class="mt-4 sm:flex-1">
                     <x-jet-label for="email" value="Correo extension ITO"/>
-                    <x-input.error wire:model="user.email" class="block mt-1 w-full" type="email" id="email" name="email" for="user.email" required/>
+                    <x-input.error wire:model.defer="user.email" class="block mt-1 w-full" type="email" id="email" name="email" for="user.email" required/>
                 </div>
 
                 <!-- Correo TECNM -->
                 <div class="mt-4 sm:flex-1">
                     <x-jet-label for="correo_tecnm" value="Correo extension TECNM"/>
-                    <x-input.error wire:model="user.correo_tecnm" class="block mt-1 w-full" type="email" id="correo_tecnm" name="correo_tecnm" for="user.correo_tecnm" required/>
+                    <x-input.error wire:model.defer="user.correo_tecnm" class="block mt-1 w-full" type="email" id="correo_tecnm" name="correo_tecnm" for="user.correo_tecnm" required/>
                 </div>
             </div>
 
@@ -104,20 +104,20 @@
                 <!-- Puesto -->
                 <div class="mt-4 sm:flex-1">
                     <x-jet-label for="puesto" value="Puesto"/>
-                    <x-input.error wire:model="user.puesto_en_area" class="block mt-1 w-full" type="text" id="puesto" name="puesto" for="user.puesto" required/>
+                    <x-input.error wire:model.defer="user.puesto_en_area" class="block mt-1 w-full" type="text" id="puesto" name="puesto" for="user.puesto" required/>
                 </div>
 
                 <!-- Hora entrada -->
                 <div class="mt-4 sm:flex-1">
                     <x-jet-label for="hora_entrada" value="Hora entrada"/>
-                    <x-jet-input wire:model="user.hora_entrada" class="block mt-1 w-full" type="time" id="hora_entrada" name="hora_entrada"/>
+                    <x-jet-input wire:model.defer="user.hora_entrada" class="block mt-1 w-full" type="time" id="hora_entrada" name="hora_entrada"/>
                     <x-jet-input-error for="user.hora_entrada"/>
                 </div>
 
                 <!-- Hora salida -->
                 <div class="mt-4 sm:flex-1">
                     <x-jet-label for="hora_salida" value="Hora salida"/>
-                    <x-jet-input wire:model="user.hora_salida" class="block mt-1 w-full" type="time" id="hora_salida" name="hora_salida"/>
+                    <x-jet-input wire:model.defer="user.hora_salida" class="block mt-1 w-full" type="time" id="hora_salida" name="hora_salida"/>
                     <x-jet-input-error for="user.hora_salida"/>
                 </div>
             </div>
@@ -125,7 +125,7 @@
             <!-- Area -->
             <div class="mt-4">
                 <x-jet-label for="area" value="Área de adscripción"/>
-                <x-input.select wire:model="user.area_id" class="mt-1 w-full" id="area_id" name="area_id" required>
+                <x-input.select wire:model.defer="user.area_id" class="mt-1 w-full" id="area_id" name="area_id" required>
                     <option value="" disabled>Selecciona el área</option>
                     @foreach(\App\Models\Area::all() as $area)
                         <option value="{{ $area->id }}">{{ $area->nombre }}</option>
@@ -138,13 +138,13 @@
                 <!-- Organización de origen -->
                 <div class="mt-4 sm:flex-1">
                     <x-jet-label for="organizacion_origen" value="Organización de origen"/>
-                    <x-input.error wire:model="user.organizacion_origen" class="block mt-1 w-full" type="text" id="organizacion_origen" name="organizacion_origen" for="user.organizacion_origen" required/>
+                    <x-input.error wire:model.defer="user.organizacion_origen" class="block mt-1 w-full" type="text" id="organizacion_origen" name="organizacion_origen" for="user.organizacion_origen" required/>
                 </div>
 
                 <!-- Cuenta moodle -->
                 <div class="mt-4 sm:flex-1">
                     <x-jet-label for="cuenta_moodle" value="Cuenta moodle"/>
-                    <x-input.select wire:model="user.cuenta_moodle" class="mt-1 w-full" id="cuenta_moodle" name="cuenta_moodle" required>
+                    <x-input.select wire:model.defer="user.cuenta_moodle" class="mt-1 w-full" id="cuenta_moodle" name="cuenta_moodle" required>
                         <option value="" disabled>Selecciona la opción</option>
                         <option value="1">Si tiene</option>
                         <option value="0">No tiene</option>
--- resources/views/livewire/admin/participants/show.blade.php
+++ resources/views/livewire/admin/participants/show.blade.php
@@ -8,13 +8,13 @@
             <!-- RFC -->
             <div class="sm:flex-1">
                 <x-jet-label value="RFC"/>
-                <x-jet-input wire:model="user.rfc" class="block mt-1 w-full" type="text" disabled/>
+                <x-jet-input wire:model.defer="user.rfc" class="block mt-1 w-full" type="text" disabled/>
             </div>
 
             <!-- CURP -->
             <div class="sm:flex-1">
                 <x-jet-label value="CURP"/>
-                <x-jet-input wire:model="user.curp" class="block mt-1 w-full" type="text" disabled/>
+                <x-jet-input wire:model.defer="user.curp" class="block mt-1 w-full" type="text" disabled/>
             </div>
         </div>
 
@@ -50,13 +50,13 @@
             <!-- Tipo -->
             <div class="mt-4 sm:flex-1">
                 <x-jet-label value="Tipo"/>
-                <x-jet-input wire:model="user.tipo" class="mt-1 w-full" type="text" disabled/>
+                <x-jet-input wire:model.defer="user.tipo" class="mt-1 w-full" type="text" disabled/>
             </div>
 
             <!-- Clave -->
             <div class="mt-4 sm:flex-1">
                 <x-jet-label value="Clave presupuestal"/>
-                <x-jet-input wire:model="user.clave_presupuestal" class="block mt-1 w-full" type="text" disabled/>
+                <x-jet-input wire:model.defer="user.clave_presupuestal" class="block mt-1 w-full" type="text" disabled/>
             </div>
         </div>
 
@@ -69,7 +69,7 @@
         <!-- Carrera-->
         <div class="mt-4">
             <x-jet-label value="Carrera"/>
-            <x-jet-input wire:model="user.carrera" class="block mt-1 w-full" type="text" disabled/>
+            <x-jet-input wire:model.defer="user.carrera" class="block mt-1 w-full" type="text" disabled/>
         </div>
 
         <!-- Correo ITO y TECNM -->
@@ -77,13 +77,13 @@
             <!-- Correo ITO -->
             <div class="mt-4 sm:flex-1">
                 <x-jet-label value="Correo extension ITO"/>
-                <x-jet-input wire:model="user.email" class="block mt-1 w-full" type="email" disabled/>
+                <x-jet-input wire:model.defer="user.email" class="block mt-1 w-full" type="email" disabled/>
             </div>
 
             <!-- Correo TECNM -->
             <div class="mt-4 sm:flex-1">
                 <x-jet-label value="Correo extension TECNM"/>
-                <x-jet-input wire:model="user.correo_tecnm" class="block mt-1 w-full" type="email" disabled/>
+                <x-jet-input wire:model.defer="user.correo_tecnm" class="block mt-1 w-full" type="email" disabled/>
             </div>
         </div>
 
@@ -92,19 +92,19 @@
             <!-- Puesto -->
             <div class="mt-4 sm:flex-1">
                 <x-jet-label value="Puesto"/>
-                <x-jet-input wire:model="user.puesto_en_area" class="block mt-1 w-full" type="text" disabled/>
+                <x-jet-input wire:model.defer="user.puesto_en_area" class="block mt-1 w-full" type="text" disabled/>
             </div>
 
             <!-- Hora entrada -->
             <div class="mt-4 sm:flex-1">
                 <x-jet-label value="Hora entrada"/>
-                <x-jet-input wire:model="user.hora_entrada" class="block mt-1 w-full" type="time"/>
+                <x-jet-input wire:model.defer="user.hora_entrada" class="block mt-1 w-full" type="time"/>
             </div>
 
             <!-- Hora salida -->
             <div class="mt-4 sm:flex-1">
                 <x-jet-label value="Hora salida"/>
-                <x-jet-input wire:model="user.hora_salida" class="block mt-1 w-full" type="time"/>
+                <x-jet-input wire:model.defer="user.hora_salida" class="block mt-1 w-full" type="time"/>
             </div>
         </div>
 
@@ -119,7 +119,7 @@
             <!-- Organización de origen -->
             <div class="mt-4 sm:flex-1">
                 <x-jet-label value="Organización de origen"/>
-                <x-jet-input wire:model="user.organizacion_origen" class="block mt-1 w-full" type="text" disabled/>
+                <x-jet-input wire:model.defer="user.organizacion_origen" class="block mt-1 w-full" type="text" disabled/>
             </div>
 
             <!-- Cuenta moodle -->
