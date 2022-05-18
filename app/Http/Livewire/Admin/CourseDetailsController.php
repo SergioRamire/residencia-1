d5f125f1f936b37f9f84d5ecb21bfafdb3531bd4
Natali-Rojas <36526810+Natali-Rojas@users.noreply.github.com>
2022-05-17 22:27:13 -0500

    Update CourseDetailsController.php

--- app/Http/Livewire/Admin/CourseDetailsController.php
+++ app/Http/Livewire/Admin/CourseDetailsController.php
@@ -20,13 +20,13 @@ class CourseDetailsController extends Component
 
     public function render()
     {
-        return view('livewire.admin.coursedetails.index',[
-            'detalles'=>CourseDetail::join('courses','courses.id','course_details.course_id')
-            ->join('groups','groups.id','course_details.group_id')
-            ->join('periods','periods.id','course_details.period_id')
-            ->select('course_details.id','course_details.lugar','course_details.capacidad',
-              'course_details.hora_inicio','course_details.hora_fin','courses.nombre as curso',
-              'groups.nombre as grupo','periods.fecha_inicio','periods.fecha_fin')
+        return view('livewire.admin.coursedetails.index', [
+            'detalles'=>CourseDetail::join('courses', 'courses.id', 'course_details.course_id')
+            ->join('groups', 'groups.id', 'course_details.group_id')
+            ->join('periods', 'periods.id', 'course_details.period_id')
+            ->select('course_details.id', 'course_details.lugar', 'course_details.capacidad',
+              'course_details.hora_inicio', 'course_details.hora_fin', 'courses.nombre as curso',
+              'groups.nombre as grupo', 'periods.fecha_inicio', 'periods.fecha_fin')
             ->orderBy('periods.fecha_inicio')
             ->paginate($this->perPage),
         ]);
