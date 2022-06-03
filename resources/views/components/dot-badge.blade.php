1a02c65baa505a3f91defd551b2733cbfdcbe140
Carlos Farid Nogales López <34426099+FaruNL@users.noreply.github.com>
2022-06-02 19:47:12 -0500

    feat: Añade número al badge de la campana

--- resources/views/components/dot-badge.blade.php
+++ resources/views/components/dot-badge.blade.php
@@ -1,4 +1,9 @@
-<span class="flex absolute h-[0.6rem] w-[0.6rem] top-1 right-1.5 pointer-events-none">
+<span class="flex absolute h-[0.8rem] w-[0.8rem] top-1 right-1.5 pointer-events-none">
     <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-indigo-400 opacity-75"></span>
-    <span class="relative inline-flex rounded-full h-[0.6rem] w-[0.6rem] bg-indigo-500"></span>
+    <span class="relative inline-flex rounded-full h-[0.8rem] w-[0.8rem] bg-indigo-500"></span>
+    <span class="absolute text-[9px] font-bold left-1">
+        @if(isset($counter))
+            <span class="text-white">{{ $counter }}</span>
+        @endif
+    </span>
 </span>
