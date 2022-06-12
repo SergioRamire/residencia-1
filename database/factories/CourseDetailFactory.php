601ca4ad0032a16862ad09b87dfecdf3a8572b44
RamirezLopezSergioSaul <sr1117747@gmail.com>
2022-06-12 00:05:16 -0500

    feat: bdv1 con notificaciones en el sistema

--- database/factories/CourseDetailFactory.php
+++ database/factories/CourseDetailFactory.php
@@ -21,10 +21,19 @@ class CourseDetailFactory extends Factory
     {
         $hora_inicio = $this->faker->time('H:i');
 
+        $areas = $this->faker->randomElement([
+            'Sala Audivisual, Edificio I',
+            'I10','I12','F1','F2','F3','F4','F5','F6',
+            'K1','K2','K3','K4','K5','K6',
+            'Sala Audivisual, Edificio B','Sala Audivisual, Edificio A',
+            'Laboratorio de inge civil',
+            'Centro de computo','H2','H3','H4','H5','H6',
+        ]);
+
         return [
             'hora_inicio' => $hora_inicio,
             'hora_fin' => date('H:i', strtotime($hora_inicio.'+1 hour')),
-            'lugar' => rtrim($this->faker->sentence(2), '.'),
+            'lugar' => $areas,
             'capacidad' => $this->faker->numberBetween(10, 30),
             'course_id' => Course::inRandomOrder()->first()->id,
             'group_id' => Group::inRandomOrder()->first()->id,
