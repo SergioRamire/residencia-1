00f74c8af486fbc365e38874969eee1cd4a5a769
RouterUIZ007 <router.uiz.360@gmail.com>
2022-06-17 13:10:54 -0500

    fix: Seeder de fecha

--- database/seeders/PeriodSeeder.php
+++ database/seeders/PeriodSeeder.php
@@ -15,8 +15,20 @@ class PeriodSeeder extends Seeder
      */
     public function run()
     {
-        Period::factory()
-            ->count(2)
-            ->create();
+        $f1 = new Period();
+        $f1->clave  ='1-JUN2022';
+        $f1->fecha_inicio ='2022-06-20';
+        $f1->fecha_fin = '2022-06-24';
+        $f1->save(); 
+
+        $f2 = new Period();
+        $f2->clave  ='2-JUN2022';
+        $f2->fecha_inicio = '2022-06-27';
+        $f2->fecha_fin = '2022-07-01';
+        $f2->save(); 
+
+        // Period::factory()
+        //     ->count(2)
+        //     ->create();
     }
 }
