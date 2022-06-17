6ba4ac3c68ae24404b183e2699b533b50834c39c
RouterUIZ007 <router.uiz.360@gmail.com>
2022-06-17 13:13:24 -0500

    feat: Cards de cursos para la vista mis cursos

--- app/Http/Livewire/Admin/StudyingController.php
+++ app/Http/Livewire/Admin/StudyingController.php
@@ -2,12 +2,42 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\CourseDetail;
 use Livewire\Component;
 
 class StudyingController extends Component
 {
+    public $user;
+    public function mount(){
+        $this->user = auth()->user();
+    }
     public function render()
     {
-        return view('livewire.admin.studying.index');
+        return view('livewire.admin.studying.index', [
+            'datos' => $this->consulta(),
+        ]);
+    }
+    public function consulta()
+    {
+        return CourseDetail::join('courses', 'courses.id', '=', 'course_details.course_id')
+            ->join('periods', 'periods.id', '=', 'course_details.period_id')
+            ->join('groups', 'groups.id', '=', 'course_details.group_id')
+            ->join('inscriptions', 'inscriptions.course_detail_id', '=', 'course_details.id')
+            ->join('users', 'users.id', '=', 'inscriptions.user_id')
+            // ->when($this->classification['periodo'], fn ($query, $search) => $query
+            //     ->where('periods.id', '=', $search))
+            ->select(
+                'courses.clave as curso_clave',
+                'courses.nombre as curso_nombre',
+                'groups.nombre as nombre_grupo',
+                // 
+                'inscriptions.calificacion as califi',
+                'periods.fecha_inicio as f1',
+                'periods.fecha_fin as f2',
+                'course_details.hora_inicio as h1',
+                'course_details.hora_fin as h2',
+            )
+            ->where("users.id", $this->user->id)
+            ->get();;
     }
 }
--- resources/views/livewire/admin/studying/index.blade.php
+++ resources/views/livewire/admin/studying/index.blade.php
@@ -1,3 +1,48 @@
 <div>
- Mis cursos
+    Mis cursos
+    {{-- <div class="grid grid-cols-4 gap-4 bg-gray-300">
+        <div class="flex flex-col w-56">
+            <div class="m-4 p-2 bg-slate-400 h-28">1</div>
+            <div class="m-4 p-2 bg-slate-400 h-28 flex flex-col justify-center">2</div>
+            <div class="m-4 p-2 bg-slate-400 h-28 flex flex-col justify-end">3</div>
+        </div>
+      </div> --}}
+
+    <div class="grid grid-cols-3">
+        @foreach ($datos as $data)
+            <div class="flex flex-col p-6 m-4 max-w-md bg-white rounded-lg border border-gray-200 shadow-md ">
+                <div class="flex flex-row">
+                    <div>{{-- Icono --}}
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
+                        </svg>
+                    </div>
+                    <div>
+                        <span class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">{{ $data->curso_clave }} -</span>
+                        <span class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">{{ $data->curso_nombre }}</span>
+                        <span class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">({{ $data->nombre_grupo }})
+                        </span>
+                    </div>
+                </div>
+
+                <div class="flex flex-col justify-center">
+                    <p class="mb-3 font-normal text-gray-700 ">Instructor: Here are the biggest enterprise</p>
+                    <p class="mb-3 font-normal text-gray-700 ">Califiacion Final: {{ $data->califi }}</p>
+                    <p>{{ $data->f1 }} - {{ $data->f2 }}</p>
+                    <p>{{ $data->h1 }} - {{ $data->h2 }}</p>
+                </div>
+
+                <div class="flex flex-col justify-end">
+                    <x-jet-secondary-button wire:click=""
+                        class="border-sky-800 text-sky-700 hover:text-sky-500 active:text-sky-800 active:bg-sky-50">
+                        <x-icon.info solid alt="sm" class="inline-block h-5 w-5" /> Ver Programa
+                    </x-jet-secondary-button>
+                </div>
+            </div>
+        @endforeach
+    </div>
 </div>
+
+
