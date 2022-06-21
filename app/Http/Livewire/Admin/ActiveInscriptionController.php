61ee3cd583e7277c10d3cef133bba42835543edd
RouterUIZ007 <router.uiz.360@gmail.com>
2022-06-20 20:38:38 -0500

    fix: Activar Inscripciones y bordes de botones

--- app/Http/Livewire/Admin/ActiveInscriptionController.php
+++ app/Http/Livewire/Admin/ActiveInscriptionController.php
@@ -2,26 +2,32 @@
 
 namespace App\Http\Livewire\Admin;
 
-use DateTime;
+use App\Models\Period;
+use Illuminate\Support\Carbon;
 use Livewire\Component;
 
 class ActiveInscriptionController extends Component
 {   
 
-    public $id_periodo;
-
-
+    public $hoy;
+    public $fecha;
+    
+    public function mount(){
+        $this->hoy = date('Y/m/d');
+        $this->fecha = $this->consulta();
+    }
+    public function consulta()
+    {
+        return Period::where('periods.fecha_inicio','>',$this->hoy)
+            ->orderBy('periods.fecha_inicio', 'asc')
+            ->first();
+    }
     public function render()
     {
         return view('livewire.admin.activeinscription.index');
     }
-    /* obtener el valor de period [id] */
-    protected $listeners = [
-        'per_send',
-    ];
-    public function per_send($valor){
-        $this->id_periodo = $valor;
-    }
+
+
     public function activar()
     {
         dd('Activaste curso');
@@ -30,7 +36,5 @@ class ActiveInscriptionController extends Component
     {
         dd('DEsactivaste curso');
     }
-    public $fecha_actual ;
-    public function fechas(){
-    }
+
 }
--- resources/views/livewire/admin/activeinscription/index.blade.php
+++ resources/views/livewire/admin/activeinscription/index.blade.php
@@ -1,9 +1,18 @@
 <div>
     <div class="p-6 w-full bg-white rounded-lg border border-sky-600 shadow-md shadow-sky-800">
-        <div>
+        <div class="m-4">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Ofertar Cursos Para las Inscripciones</h5>
         </div>
-        {{-- {{$date}} --}}
+        <div class="m-4">
+            <p class="text-xl mx=4">Fecha de hoy <span class="font-bold">{{ date('d-m-Y', strtotime($hoy)) }}</span></p>
+            <p class="text-xl mx=4">Periodo mas cercano: <span class="font-bold">
+                    {{ date('d-m-Y', strtotime($fecha->fecha_inicio)) }}
+                    -  
+                    {{ date('d-m-Y', strtotime($fecha->fecha_fin)) }}
+                </span>
+            </p>
+        </div>
+
         <div class="mb-3 font-normal text-gray-700 dark:text-gray-400">
             <div class="flex justify-center ">
                 <x-jet-secondary-button wire:click="activar()"
