82c031fdf4e43f402946d63046be34963f23e6fa
RouterUIZ007 <router.uiz.360@gmail.com>
2022-06-29 18:49:25 -0500

    fix: historial curso, login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,22 +11,16 @@
         </x-slot>
 
         <x-slot name="boton">
-            <button wire:click="" 
-            class="inline-flex items-center px-4 py-2 bg-white 
-            rounded-t-lg font-semibold text-xs uppercase tracking-widest shadow-sm 
-            focus:outline-none focus:ring 
-            focus:ring-blue-200 disabled:opacity-25 transition
-            text-sky-700 hover:text-white hover:bg-sky-800 active:text-sky-50 active:bg-sky-500">
-                Participantes
-            </button>
-            <button wire:click="" 
-            class="inline-flex items-center px-4 py-2 bg-white 
-            rounded-t-lg font-semibold text-xs uppercase tracking-widest shadow-sm 
-            focus:outline-none focus:ring 
-            focus:ring-blue-200 disabled:opacity-25 transition
-            text-sky-700 hover:text-white hover:bg-sky-800 active:text-sky-50 active:bg-sky-500">
-            Instructor
-            </button>
+            <div class="flex flex-wrap">
+                <div class="flex items-center p-2 bg-white rounded-t-lg font-semibold text-xs uppercase tracking-widest shadow-sm focus:outline-none focus:ring focus:ring-blue-200 disabled:opacity-25 transition text-sky-700 ">
+                    <input checked name="rol-radio" value="Participante" id="par-radio" type="radio"  class="w-4 h-4 text-sky-800 bg-gray-100 border-gray-300 focus:ring-sky-700 focus:ring-2 ">
+                    <label for="par-radio" class="ml-2 text-sm font-medium text-gray-900 ">Participante</label>
+                </div>
+                <div class="flex items-center p-2 bg-white rounded-t-lg font-semibold text-xs uppercase tracking-widest shadow-sm focus:outline-none focus:ring focus:ring-blue-200 disabled:opacity-25 transition text-sky-700 ">
+                    <input name="rol-radio" value="Instructor" id="ins-radio" type="radio" class="w-4 h-4 text-orange-600 bg-gray-100 border-gray-300 focus:ring-orange-500 focus:ring-2">
+                    <label for="ins-radio" class="ml-2 text-sm font-medium text-gray-900 ">Instructor</label>
+                </div>
+            </div>
         </x-slot>
 
 
