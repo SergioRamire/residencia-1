cd11275f8ad3b75ab1aaf0bdcc90f556124b547d
RouterUIZ007 <router.uiz.360@gmail.com>
2022-06-30 02:25:29 -0500

    style: Card Mis cursos

--- resources/views/livewire/admin/teaching/index.blade.php
+++ resources/views/livewire/admin/teaching/index.blade.php
@@ -4,7 +4,7 @@
     <div class="grid grid-flow-row xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1">
         {{-- <div class="grid grid-cols-3 md:grid-cols-max"> --}}
         @foreach ($datos as $data)
-            <div class="flex flex-col m-2 max-w-md bg-#white rounded-lg border shadow-md ">
+            <div class="flex flex-col justify-between m-2 max-w-md bg-#white rounded-lg border shadow-md transition ease-in-out bg-white hover:-translate-y-1 hover:scale-105 hover:bg-blue-50 duration-300" style="height: 350px">
                 <div class="flex flex-row p-2 m-4 bg-white text-black border shadow-md rounded-lg">
                     <div class="mr-2 text-[#1b396a] h-12 w-12">{{-- Icono --}}
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
@@ -23,7 +23,6 @@
                     <p><span class="font-bold">Periodo: </span>{{ date('d-m-Y', strtotime($data->f1))}} - {{ date('d-m-Y', strtotime($data->f2))}}</p>
                     <p><span class="font-bold">Horaio: </span>{{ date("H:i", strtotime($data->h1))}} - {{ date("H:i", strtotime($data->h2))}} </p>
                 </div>
-                
             </div>
         @endforeach
     </div>
