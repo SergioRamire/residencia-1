9d6ad0f8ab7ce2122fd94d8519e34e5f457de2a7
Carlos Farid Nogales López <34426099+FaruNL@users.noreply.github.com>
2022-07-04 14:57:14 -0500

    fix: Actualiza la instancia de flatpickr según el valor de la hora

--- resources/views/components/datepicker.blade.php
+++ resources/views/components/datepicker.blade.php
@@ -1,9 +1,14 @@
 @props(['disabled' => false, 'ref' => 'myDatePicker',  'config' => false])
 
 <div>
-    <input type="text" id="{{ $ref }}" {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-sky-800 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm'])->except(['type']) !!}>
+    <input x-data="{
+        value: @entangle($attributes->wire('model')),
+        instance: undefined,
+        init() {
+            $watch('value', value => this.instance.setDate(value, false));
+            this.instance = flatpickr({!! $ref !!}, {!! $config ? htmlspecialchars(json_encode($config)) : '{}' !!});
+        }
+    }"
+           type="text"
+           id="{{ $ref }}" {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-sky-800 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm'])->except(['type']) !!}>
 </div>
-
-<script>
-    flatpickr({!! $ref !!}, {!! $config ? json_encode($config) : "{}" !!});
-</script>
