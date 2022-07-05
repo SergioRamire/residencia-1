976170ebc2dee5a3748170ef4c8e7178a1ee74ca
RouterUIZ007 <router.uiz.360@gmail.com>
2022-07-04 23:59:22 -0500

    fix: Update Area

--- app/Http/Livewire/Admin/AreaController.php
+++ app/Http/Livewire/Admin/AreaController.php
@@ -2,123 +2,93 @@
 
 namespace App\Http\Livewire\Admin;
 
-use App\Http\Traits\WithSorting;
 use App\Models\Area;
-use Carbon\Carbon;
 use Livewire\Component;
 use Livewire\WithPagination;
+use App\Http\Traits\WithSorting;
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class AreaController extends Component
 {
     use WithPagination;
     use WithSorting;
+    use AuthorizesRequests;
 
-    public Area $area;
-    public $perPage = '5';
-    public $search = '';
-    public $area_id;
-    public $nombre;
-    public $telefono;
-    public $jefe_area;
-    public $extension;
-    public $clave;
+    public Area $areas;
     public $edit = false;
     public $create = false;
-
-    // public $date;
-    // public $data2 = date('Y-m-d H:i:s');
-
-    protected $queryString = [
-        'search' => ['except' => '', 'as' => 's'],
-        'perPage' => ['except' => 1, 'as' => 'p'],
-    ];
 
     public $showEditCreateModal = false;
     public $confirmingAreaDeletion = false;
     public $confirmingSaveArea = false;
 
-    public function rules(): array
-    {
+    public $perPage = '5';
+    public $search = '';
+    protected $queryString = [
+        'search' => ['except' => '', 'as' => 's'],
+        'perPage' => ['except' => 1, 'as' => 'p'],
+    ];
+
+    public function rules(): array{
         if ($this->edit) {
             return [
-                'nombre' => ['required', 'regex:/^[\pL\pM\s]+$/u'],
-                'jefe_area' => ['required', 'regex:/^[\pL\pM\s]+$/u'],
-                'extension' => ['required', 'numeric'],
-                'clave' => ['required', 'alpha_num'],
-                'telefono' => ['required', 'numeric'],
-            ];
-        }
-
+                'areas.nombre' => ['required', 'regex:/^[\pL\pM\s]+$/u'],
+                'areas.jefe_area' => ['required', 'regex:/^[\pL\pM\s]+$/u'],
+                'areas.extension' => ['required', 'numeric'],
+                'areas.clave' => ['required', 'alpha_num'],
+                'areas.telefono' => ['required', 'numeric'],
+        ];}
         return [
-            'nombre' => ['required', 'regex:/^[\pL\pM\s]+$/u', 'unique:areas'],
-            'jefe_area' => ['required', 'regex:/^[\pL\pM\s]+$/u'],
-            'extension' => ['required', 'numeric'],
-            'clave' => ['required', 'alpha_num', 'unique:areas'],
-            'telefono' => ['required', 'numeric'],
+            'areas.nombre' => ['required', 'regex:/^[\pL\pM\s]+$/u', 'unique:areas'],
+            'areas.jefe_area' => ['required', 'regex:/^[\pL\pM\s]+$/u'],
+            'areas.extension' => ['required', 'numeric'],
+            'areas.clave' => ['required', 'alpha_num', 'unique:areas'],
+            'areas.telefono' => ['required', 'numeric'],
         ];
     }
 
-    public function mount()
-    {
-        // $this->date  = Carbon::now();
+    public function mount(){
+        $this->blankArea();
     }
 
-    public function updated($propertyName)
-    {
+    public function blankArea(){
+        $this->areas = Area::make();
+    }
+
+    public function updated($propertyName){
         $this->validateOnly($propertyName);
     }
 
-    public function updatingSearch()
-    {
+    public function updatingSearch(){
         $this->resetPage();
     }
 
-    public function resetFilters()
-    {
+    public function resetFilters(){
         $this->reset('search');
     }
 
-    public function create()
-    {
+    public function create(){
         $this->resetErrorBag();
-        $this->resetInputFields();
+        $this->blankArea();
         $this->openModal();
         $this->edit = false;
         $this->create = true;
     }
 
-    public function openModal()
-    {
+    public function openModal(){
         $this->showEditCreateModal = true;
     }
 
-    public function closeModal()
-    {
+    public function closeModal(){
         $this->showEditCreateModal = false;
     }
 
-    private function resetInputFields()
-    {
-        $this->area_id = '';
-        $this->clave = '';
-        $this->nombre = '';
-        $this->jefe_area = '';
-        $this->telefono = '';
-        $this->extension = '';
-    }
 
-    public function store()
+    public function save()
     {
         $this->validate();
-
-        Area::updateOrCreate(['id' => $this->area_id], [
-            'clave' => $this->clave,
-            'nombre' => $this->nombre,
-            'jefe_area' => $this->jefe_area,
-            'telefono' => $this->telefono,
-            'extension' => $this->extension,
-        ]);
-
+        $this->areas->save();
         $this->dispatchBrowserEvent('notify', [
             'icon' => $this->edit ? 'pencil' : 'success',
             'message' =>  $this->edit ? 'Área actualizada exitosamente' : 'Área creada exitosamente',
@@ -132,7 +102,6 @@ class AreaController extends Component
         $this->resetValidation();
 
         $this->closeModal();
-        $this->resetInputFields();
     }
 
     public function updateArea()
@@ -147,40 +116,32 @@ class AreaController extends Component
          $this->resetErrorBag();
          $this->resetValidation();
 
-        $area = Area::findOrFail($id);
-        $this->area_id = $id;
-        $this->clave = $area->clave;
-        $this->nombre = $area->nombre;
-        $this->jefe_area = $area->jefe_area;
-        $this->telefono = $area->telefono;
-        $this->extension = $area->extension;
+        $this->areas = Area::findOrFail($id);
         $this->edit = true;
         $this->create = false;
         $this->openModal();
     }
 
-    public function deleteArea($id, $nombr)
+    public function deleteArea($id)
     {
-        $this->area = Area::findOrFail($id);
-        $this->nombre = $nombr;
+        $this->areas = Area::findOrFail($id);
         $this->confirmingAreaDeletion = true;
     }
 
     public function delete()
     {
-        $this->area->delete();
+        $this->areas->delete();
         $this->confirmingAreaDeletion = false;
         $this->dispatchBrowserEvent('notify', [
             'icon' => 'trash',
             'message' =>  'Área eliminada exitosamente',
         ]);
-        $this->resetInputFields();
     }
 
     public function render()
     {
         return view('livewire.admin.areas.index', [
-            'areas' => Area::where('nombre', 'like', '%'.$this->search.'%')
+            'datosareas' => Area::where('nombre', 'like', '%'.$this->search.'%')
                             ->orWhere('jefe_area', 'like', '%'.$this->search.'%')
                             ->orWhere('clave', 'like', '%'.$this->search.'%')
                             ->orWhere('telefono', 'like', '%'.$this->search.'%')
--- resources/views/livewire/admin/areas/index.blade.php
+++ resources/views/livewire/admin/areas/index.blade.php
@@ -61,7 +61,7 @@
                     <x-table.header>acciones</x-table.header>
                 </x-slot>
 
-                @forelse($areas as $a)
+                @forelse($datosareas as $a)
                     <tr wire:key="area-{{ $a->id }}" wire:loading.class.delay="opacity-50">
                         <x-table.cell>{{ $a->clave }}</x-table.cell>
                         <x-table.cell>{{ $a->nombre }}</x-table.cell>
@@ -72,7 +72,7 @@
                             <button  wire:click="edit({{ $a->id }})" type="button" class="px-4 bg-white hover:text-white hover:bg-amber-500 text-black font-bold border border-amber-400 rounded shadow" >
                                 Editar
                             </button>
-                            <button wire:click="deleteArea('{{ $a->id }}','{{ $a->nombre }}')" type="button" class="px-4 bg-white hover:text-white hover:bg-red-600 text-black font-bold border border-red-400 rounded shadow">
+                            <button wire:click="deleteArea('{{ $a->id }}')" type="button" class="px-4 bg-white hover:text-white hover:bg-red-600 text-black font-bold border border-red-400 rounded shadow">
                                 Eliminar
                             </button>
                         </x-table.cell>
@@ -95,7 +95,7 @@
                 @endforelse
             </x-table>
             <div>
-                {{ $areas->links() }}
+                {{ $datosareas->links() }}
             </div>
             @if($create)
                         @include('livewire.admin.areas.edit_create',['modo'=>'Crear'])
