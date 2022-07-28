b80b7425fb0068ff7e72a7fe86660788776cb8d0
Carlos Farid Nogales López <34426099+FaruNL@users.noreply.github.com>
2022-07-27 21:54:38 -0500

    feat: Programa la ejecución del backup: Cada lunes 07:00
    Corrige la zona horaria a México.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('snapshot:create')->weekly()->mondays()->at('07:00');
     }
 
     /**
