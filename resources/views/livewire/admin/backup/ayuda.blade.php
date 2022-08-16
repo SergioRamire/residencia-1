57ec258696d586e80eb546e0406c2c4cf90d3945
RouterUIZ007 <router.uiz.360@gmail.com>
2022-08-16 10:29:38 -0500

    feat: Ayuda con desplegables en Boton ayuda

--- resources/views/livewire/admin/backup/ayuda.blade.php
+++ resources/views/livewire/admin/backup/ayuda.blade.php
@@ -4,50 +4,90 @@
     </x-slot>
     <x-slot name="content">
 
-        <div class="mx-8 my-4 grid grid-rows-1">
-            <ul>
-                <li><span class="font-bold text-justify">Pasos para crear Respaldo de la base de datos.</span></li>
-                <li><span class="font-normal text-justify">Presionar el boton <strong class="bg-white text-black font-bold border border-[#1b396a] rounded shadow">NUEVO RESPALDO</strong>.</span></li>
-                <li><span class="font-normal text-justify">Aparecera un modal, presionar el boton CREAR.</span></li>
-                <li><span class="font-normal text-justify">El respaldo se generara, y se mostrara en la tabla.</span></li>
-            </ul>
+        <div x-data="{ open: false }" @click="open = true" class="p-4 my-4 bg-gray-100 border-gray-100 rounded-md">
+            <div class="flex justify-between">
+                <button class="font-bold text-justify text-xl">Pasos para crear Respaldo de la base de datos.</button> 
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+                </svg>
+            </div>
+
+            <div x-show="open" @click.outside="open = false">
+                <div class="py-2 px-4">
+                    <span class="font-normal text-justify text-lg">Presionar el boton <strong class="bg-white text-black font-bold border border-[#1b396a] rounded shadow">NUEVO RESPALDO</strong>.</span>
+                </div>
+                <div class="py-2 px-4">
+                    <span class="font-normal text-justify text-lg">Aparecera un modal, presionar el boton <strong class="bg-[#1b396a] text-white font-bold border border-[#1b396a] rounded shadow">Crear</strong>.</span>
+                    
+                </div>
+                <div class="py-2 px-4">
+                    <span class="font-normal text-justify text-lg">El respaldo se generara, y se mostrara en la tabla.</span>
+                </div>
+            </div>
         </div>
 
-        <div class="mx-8 my-4 grid grid-rows-1">
-            <ul>
-                <li><span class="font-bold text-justify">Pasos para descargar Respaldo de la base de datos.</span></li>
-                <li><span class="font-normal text-justify">Presionar el boton <strong class="bg-white text-black font-bold border border-amber-400 rounded shadow">Descargar</strong>.</span></li>
-                <li><span class="font-normal text-justify">Elegimos donde guardar, y empezara a descargar el respaldo.</span></li>
-            </ul>
+        <div x-data="{ open: false }" @click="open = true" class="p-4 my-4 bg-gray-100 border-gray-100 rounded-md">
+            <div class="flex justify-between">
+                <button class="font-bold text-justify text-xl">Pasos para descargar Respaldo de la base de datos.</button>
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+                </svg>
+            </div>
+            <div x-show="open" @click.outside="open = false">
+                <div class="py-2 px-4">
+                    <span class="font-normal text-justify text-lg">Presionar el boton <strong class="bg-white text-black font-bold border border-amber-400 rounded shadow">Descargar</strong>.</span>
+                </div>
+                <div class="py-2 px-4">
+                    <span class="font-normal text-justify text-lg">Elegimos donde guardar, y empezara a descargar el respaldo.</span>
+                </div>
+            </div>
         </div>
 
-        <div class="mx-8 my-4 grid grid-rows-1">
-            <ul>
-                <li><span class="font-bold text-justify">Pasos para respaldar la base de datos.</span></li>
-                <li><span class="font-normal text-justify">Presionar el boton <strong class="bg-white text-black font-bold border border-green-400 rounded shadow">RESTAURAR</strong>.</span></li>
-                <li><span class="font-normal text-justify">Aparecera un modal, presionar el boton RESTAURAR.</span></li>
-                <li><span class="font-normal text-justify">El respaldo se estara ejecutando, al terminar los datos estaran resaurados.</span></li>
-            </ul>
+        <div x-data="{ open: false }" @click="open = true" class="p-4 my-4 bg-gray-100 border-gray-100 rounded-md">
+            <div class="flex justify-between">
+                <button class="font-bold text-justify text-xl">Pasos para respaldar la base de datos.</button>
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+                </svg>
+            </div>
+            <div x-show="open" @click.outside="open = false">
+                <div class="py-2 px-4">
+                    <span class="font-normal text-justify text-lg">Presionar el boton <strong class="bg-white text-black font-bold border border-green-400 rounded shadow">Restaurar</strong>.</span>
+                </div>
+                <div class="py-2 px-4">
+                    <span class="font-normal text-justify text-lg">Aparecera un modal, presionar el boton <strong class="bg-green-800 text-white font-bold border border-green-400 rounded shadow">Restaurar</strong>.</span>
+                </div>
+                <div class="py-2 px-4">
+                    <span class="font-normal text-justify text-lg">El respaldo se estara ejecutando, al terminar los datos estaran resaurados.</span>
+                </div>
+            </div>
         </div>
-        
-        <div class="mx-8 my-4 grid grid-rows-1">
-            <ul>
-                <li><span class="font-bold text-justify">Pasos para eliminar el respaldo de la base de datos.</span></li>
-                <li><span class="font-normal text-justify">Presionar el boton <strong class="bg-white text-black font-bold border border-red-400 rounded shadow">ELIMINAR</strong>.</span></li>
-                <li><span class="font-normal text-justify">Aparecera un modal, presionar el boton ELIMINAR.</span></li>
-                <li><span class="font-normal text-justify">El respaldo se eliminara, y ya no aparecesar en la tabla.</span></li>
-            </ul>
+
+        <div x-data="{ open: false }" @click="open = true" class="p-4 my-4 bg-gray-100 border-gray-100 rounded-md">
+            <div class="flex justify-between">
+                <button class="font-bold text-justify text-xl">Pasos para eliminar el respaldo de la base de datos.</button>
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+                </svg>
+            </div>
+            <div x-show="open" @click.outside="open = false">
+                <div class="p-4">
+                    <span class="font-normal text-justify text-lg">Presionar el boton <strong class="bg-white text-black font-bold border border-red-400 rounded shadow">Eliminar</strong>.</span>
+                </div>
+                <div class="py-2 px-4">
+                    <span class="font-normal text-justify text-lg">Aparecera un modal, presionar el boton <strong class="bg-red-800 text-white font-bold border border-red-400 rounded shadow">Eliminar</strong>.</span>
+                </div>
+                <div class="py-2 px-4">
+                    <span class="font-normal text-justify text-lg">El respaldo se eliminara, y ya no aparecesar en la tabla.</span>
+                </div>
+            </div>
         </div>
-        
-    </x-slot>
+
+    </x-slot>   
 
     <x-slot name="footer">
         <x-jet-secondary-button wire:click="$toggle('modal_ayuda')" wire:loading.attr="disabled">
-            Aceptar
+            Cerrar
         </x-jet-secondary-button>
-
-        <x-jet-button class="ml-3" wire:click.prevent="" wire:loading.attr="disabled" form="courseForm">
-            algo mas
-         </x-jet-button>
     </x-slot>
 </x-jet-dialog-modal>
