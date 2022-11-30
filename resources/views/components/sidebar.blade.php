0112607bae8bd0e154a0065dc5a909427d5246d9
RouterUIZ007 <router.uiz.360@gmail.com>
2022-11-29 23:22:40 -0600

    fix: Arreglo de estado en periodo

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -177,7 +177,7 @@ $route = $role === 'super-admin' ? '' : "{$role}." ;
                 </x-sidebar.link>
                 @endcan
 
-                @can('qualification.edit')
+                @can('qualification.show')
                 <x-sidebar.link :href='route("instr.grades")' :active="request()->routeIs('instr.grades')">
                     <x-slot name="icon">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
@@ -185,7 +185,6 @@ $route = $role === 'super-admin' ? '' : "{$role}." ;
                     Asignar Calificaciones
                 </x-sidebar.link>
                 @endcan
-
 
                 @can('constancias.show')
                 <x-sidebar.dropdown title="Generar constancias" dp-id="1">
@@ -416,17 +415,17 @@ $route = $role === 'super-admin' ? '' : "{$role}." ;
                 @endcan
 
                 @can('constancias.show')
-                    <x-sidebar.dropdown title="Generar constancias" dp-id="1">
-                        <x-slot name="icon" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
-                        </x-slot>
-                        <x-sidebar.link class="pl-11" :href='route("admin.constancias")' :active="request()->routeIs('admin.constancias')">
-                            Participantes
-                        </x-sidebar.link>
-                        <x-sidebar.link class="pl-11" :href='route("admin.constanciasinstructores")' :active="request()->routeIs('admin.constanciasinstructores')">
-                            Instructores
-                        </x-sidebar.link>
-                    </x-sidebar.dropdown>
+                <x-sidebar.dropdown title="Generar constancias" dp-id="1">
+                    <x-slot name="icon" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+                    </x-slot>
+                    <x-sidebar.link class="pl-11" :href='route("admin.constancias")' :active="request()->routeIs('admin.constancias')">
+                        Participantes
+                    </x-sidebar.link>
+                    <x-sidebar.link class="pl-11" :href='route("admin.constanciasinstructores")' :active="request()->routeIs('admin.constanciasinstructores')">
+                        Instructores
+                    </x-sidebar.link>
+                </x-sidebar.dropdown>
                 @endcan
 
                 @can('const_firmada.show')
